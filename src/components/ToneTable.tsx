0b73fbc0b8f4944cc93b6b3bca9f4942b19eaf2d
graphemecluster <graphemecluster@gmail.com>
2026-02-22 05:49:02 +0800

    Add arrows to tone table

--- src/components/ToneTable.tsx
+++ src/components/ToneTable.tsx
@@ -15,127 +15,127 @@ const ToneTable = () => {
       <tr>
         <td>1</td>
         <td>
-          11 <Audio id="11" />
+          1￫1 <Audio id="11" />
         </td>
         <td>
-          12 <Audio id="12" />
+          1￫2 <Audio id="12" />
         </td>
         <td>
-          13 <Audio id="13" />
+          1￫3 <Audio id="13" />
         </td>
         <td>
-          14 <Audio id="14" />
+          1￫4 <Audio id="14" />
         </td>
         <td>
-          15 <Audio id="15" />
+          1￫5 <Audio id="15" />
         </td>
         <td>
-          16 <Audio id="16" />
+          1￫6 <Audio id="16" />
         </td>
       </tr>
       <tr>
         <td>2</td>
         <td>
-          21 <Audio id="21" />
+          2￫1 <Audio id="21" />
         </td>
         <td>
-          22 <Audio id="22" />
+          2￫2 <Audio id="22" />
         </td>
         <td>
-          23 <Audio id="23" />
+          2￫3 <Audio id="23" />
         </td>
         <td>
-          24 <Audio id="24" />
+          2￫4 <Audio id="24" />
         </td>
         <td>
-          25 <Audio id="25" />
+          2￫5 <Audio id="25" />
         </td>
         <td>
-          26 <Audio id="26" />
+          2￫6 <Audio id="26" />
         </td>
       </tr>
       <tr>
         <td>3</td>
         <td>
-          31 <Audio id="31" />
+          3￫1 <Audio id="31" />
         </td>
         <td>
-          32 <Audio id="32" />
+          3￫2 <Audio id="32" />
         </td>
         <td>
-          33 <Audio id="33" />
+          3￫3 <Audio id="33" />
         </td>
         <td>
-          34 <Audio id="34" />
+          3￫4 <Audio id="34" />
         </td>
         <td>
-          35 <Audio id="35" />
+          3￫5 <Audio id="35" />
         </td>
         <td>
-          36 <Audio id="36" />
+          3￫6 <Audio id="36" />
         </td>
       </tr>
       <tr>
         <td>4</td>
         <td>
-          41 <Audio id="41" />
+          4￫1 <Audio id="41" />
         </td>
         <td>
-          42 <Audio id="42" />
+          4￫2 <Audio id="42" />
         </td>
         <td>
-          43 <Audio id="43" />
+          4￫3 <Audio id="43" />
         </td>
         <td>
-          44 <Audio id="44" />
+          4￫4 <Audio id="44" />
         </td>
         <td>
-          45 <Audio id="45" />
+          4￫5 <Audio id="45" />
         </td>
         <td>
-          46 <Audio id="46" />
+          4￫6 <Audio id="46" />
         </td>
       </tr>
       <tr>
         <td>5</td>
         <td>
-          51 <Audio id="51" />
+          5￫1 <Audio id="51" />
         </td>
         <td>
-          52 <Audio id="52" />
+          5￫2 <Audio id="52" />
         </td>
         <td>
-          53 <Audio id="53" />
+          5￫3 <Audio id="53" />
         </td>
         <td>
-          54 <Audio id="54" />
+          5￫4 <Audio id="54" />
         </td>
         <td>
-          55 <Audio id="55" />
+          5￫5 <Audio id="55" />
         </td>
         <td>
-          56 <Audio id="56" />
+          5￫6 <Audio id="56" />
         </td>
       </tr>
       <tr>
         <td>6</td>
         <td>
-          61 <Audio id="61" />
+          6￫1 <Audio id="61" />
         </td>
         <td>
-          62 <Audio id="62" />
+          6￫2 <Audio id="62" />
         </td>
         <td>
-          63 <Audio id="63" />
+          6￫3 <Audio id="63" />
         </td>
         <td>
-          64 <Audio id="64" />
+          6￫4 <Audio id="64" />
         </td>
         <td>
-          65 <Audio id="65" />
+          6￫5 <Audio id="65" />
         </td>
         <td>
-          66 <Audio id="66" />
+          6￫6 <Audio id="66" />
         </td>
       </tr>
     </table>
